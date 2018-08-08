dcf641a6a03cdb27f3f87e8840ed5d8e4974c5c8
Pete Whittle <peterwhittle75@gmail.com>
2018-08-08 17:03:49 +0100

    App updated with a few more features

--- client/src/app.js
+++ client/src/app.js
@@ -1,19 +1,25 @@
 const BucketListView = require('./views/bucketListView.js');
 const CountryListView = require('./views/countryListView.js');
+const Request = require('./services/request.js');
 
 const bucketListView = new BucketListView();
 const countryListView = new CountryListView();
 
+const countryRequest = new Request('https://restcountries.eu/rest/v2/all');
+const bucketRequest = new Request('http://localhost:300/api/bucket_countries');
+
+
 const getAllBucketListCountries = function(allCountries){
   for(country of allCountries){
-
+    bucketListView.addCountry(country);
   }
 }
 
-
 const appStart = function(){
-  request.get(getAllBucketListCountries);
+  console.log('Hello world!')
+  // request.get(getAllBucketListCountries);
+  // request.get(countryRequest);
+  debugger;
 }
-
 
 document.addEventListener('DOMContentLoaded', appStart);
