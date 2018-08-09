1a7f74d7df471c20ce2866a83683563bdc4153e6
Pete Whittle <peterwhittle75@gmail.com>
2018-08-09 13:25:41 +0100

    CSS

--- client/src/views/mapWrapper.js
+++ client/src/views/mapWrapper.js
@@ -2,8 +2,8 @@ const MapWrapper = function(containerID, coords, zoom) {
   const osmLayer = new L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	subdomains: 'abcd',
-	minZoom: 1,
-	maxZoom: 16,
+	minZoom: 2,
+	maxZoom: 15,
 	ext: 'png'
 });
   this.map = L.map(containerID).addLayer(osmLayer).setView(coords, zoom);
